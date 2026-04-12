fccf064352856d8cbaaabe5b9daa35b267d8b997
Nguyễn Lâm Hoàng Nhật <167016380+beartobear@users.noreply.github.com>
2026-04-12 10:30:32 +0700

    Update baocao.js

--- static/js/baocao.js
+++ static/js/baocao.js
@@ -1,14 +1,24 @@
 $(document).ready(function() {
+
     $('#lopSelect').change(function() {
         var lop_id = $(this).val();
-        $('#btnXemBaoCao, #btnXuatExcel').prop('disabled', !lop_id);
+        $('#btnXemBaoCao, #btnXuatExcel, #btnXuatExcelThue').prop('disabled', !lop_id);
     });
+
     $('#btnXemBaoCao').click(function() {
         var lop_id = $('#lopSelect').val();
         if(lop_id) window.location.href = '/bao-cao/lop/' + lop_id;
     });
+
     $('#btnXuatExcel').click(function() {
         var lop_id = $('#lopSelect').val();
         if(lop_id) window.location.href = '/export/excel/lop/' + lop_id;
     });
+
+    // NEW
+    $('#btnXuatExcelThue').click(function() {
+        var lop_id = $('#lopSelect').val();
+        if(lop_id) window.location.href = '/bao-cao/lop/' + lop_id + '?mode=select';
+    });
+
 });
